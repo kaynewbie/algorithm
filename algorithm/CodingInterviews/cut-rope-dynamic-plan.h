f1fd1381421b667a8de655341dedab7f2ebeabb8
Kai <kaynewbie@gmail.com>
2019-07-18 06:43:02 +0800

    feat: cut rope in dynamic plan

---
+++ algorithm/CodingInterviews/cut-rope-dynamic-plan.h
@@ -0,0 +1,16 @@
+//
+//  cut-rope-dynamic-plan.h
+//  algorithm
+//
+//  Created by Kai on 2019/7/18.
+//  Copyright © 2019 kai. All rights reserved.
+//
+
+#ifndef cut_rope_dynamic_plan_h
+#define cut_rope_dynamic_plan_h
+
+#include <stdio.h>
+
+void testMaxProduct(void);
+
+#endif /* cut_rope_dynamic_plan_h */
