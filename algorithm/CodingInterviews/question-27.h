a19d9b20f685aa69888cb437d5c1ae9675a66715
Kai <kaynewbie@gmail.com>
2019-08-03 14:33:06 +0800

    feat: coding interview question 27

---
+++ algorithm/CodingInterviews/question-27.h
@@ -0,0 +1,16 @@
+//
+//  question-27.h
+//  algorithm
+//
+//  Created by Kai on 2019/8/3.
+//  Copyright © 2019 kai. All rights reserved.
+//
+
+#ifndef question_27_h
+#define question_27_h
+
+#include <stdio.h>
+
+void testMirrorBinary(void);
+
+#endif /* question_27_h */
