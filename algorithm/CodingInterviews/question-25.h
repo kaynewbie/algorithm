f915c64c25ec4eecbf92509d2b1d78d6f25a6214
Kai <kaynewbie@gmail.com>
2019-08-03 10:48:23 +0800

    feat: coding interview question 25

---
+++ algorithm/CodingInterviews/question-25.h
@@ -0,0 +1,14 @@
+//
+//  question-25.h
+//  algorithm
+//
+//  Created by Kai on 2019/8/3.
+//  Copyright © 2019 kai. All rights reserved.
+//
+
+#ifndef question_25_h
+#define question_25_h
+
+#include <stdio.h>
+
+#endif /* question_25_h */
