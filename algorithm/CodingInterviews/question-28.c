b4d978cd0d46b9c0ac30c4c8f9c20e773ee8e878
Kai <kaynewbie@gmail.com>
2019-08-04 13:39:12 +0800

    feat: coding interview question 28

---
+++ algorithm/CodingInterviews/question-28.c
@@ -0,0 +1,15 @@
+//
+//  question-28.c
+//  algorithm
+//
+//  Created by Kai on 2019/8/4.
+//  Copyright © 2019 kai. All rights reserved.
+//
+
+#include "question-28.h"
+
+/*
+ 对称二叉树
+ 
+ LeetCode -> Easy -> symmetric-tree
+ */
---
+++ algorithm/CodingInterviews/question-28.h
@@ -0,0 +1,14 @@
+//
+//  question-28.h
+//  algorithm
+//
+//  Created by Kai on 2019/8/4.
+//  Copyright © 2019 kai. All rights reserved.
+//
+
+#ifndef question_28_h
+#define question_28_h
+
+#include <stdio.h>
+
+#endif /* question_28_h */
